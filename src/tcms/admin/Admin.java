ea1cb0cbcb093c9c9101bbe661599b370042f623
ReasonalWork <reasonalworkplace@gmail.com>
2025-06-16 13:02:08 +0800

    Changed Admin.java directory

--- src/tcms/admin/Admin.java
+++ src/tcms/admin/Admin.java
@@ -1,9 +1,4 @@
-package tcms.users;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+package tcms.admin;
 
 public class Admin {
 	private static int idNumber = 1;
@@ -11,45 +6,52 @@ public class Admin {
 	private int userID;
 	private String email;
 	private String address;
-	
+
 	public Admin() {
-		
+
 	}
-	
+
 	public String getAdminID() {
 		return adminID;
 	}
-	
+
 	public static String advanceAdminID() {
 		String ID;
 		String prefix = "A";
 		int number = idNumber;
-		
+
 		idNumber++;
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append(prefix).append(String.valueOf(number));
 		ID = stringBuilder.toString();
 		return ID;
-		
+
 	}
+
 	public void setAdminID(String adminID) {
 		this.adminID = adminID;
 	}
+
 	public int getUserID() {
 		return userID;
 	}
+
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
