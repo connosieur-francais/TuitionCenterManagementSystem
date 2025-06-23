c00a7a99d01bbef6f0a1376cff6cfcfbb7d7de41
ReasonalWork <reasonalworkplace@gmail.com>
2025-06-23 22:37:17 +0800

    Added GenerateReport Gui

--- src/tcms/admin/generateReport.java
+++ src/tcms/admin/generateReport.java
@@ -2,100 +2,102 @@ package tcms.admin;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.YearMonth;
+import java.util.Calendar;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.ImageIcon;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.Dimension;
+import javax.swing.SwingConstants;
 
-public class generateReport extends JFrame {
+public class generateReport extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JFrame frame = new JFrame();
-	private JPanel topBorder;
-	
+	private JPanel topBorder, leftBorder, bottomBorder, rightBorder;
+
+	private JLabel monthEndedlabel, atcLogo;
+	private JLabel incomeStatementLabel;
+	private JPanel incomeStatementPanel;
 	private Color borderColor = new Color(44, 44, 122);
 
 //	/**
 //	 * Launch the application.
 //	 */
 //	public static void main(String[] args) {
-//				try {
-//					generateReport frame = new generateReport("December", "2025");
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
+//		try {
+//			new generateReport("December", "2025");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 //	}
 
 	/**
 	 * Create the frame.
 	 */
 	public generateReport(String month, String year) {
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setBounds(100, 100, 900, 700);
 		frame.setResizable(false);
 		frame.setTitle(String.format("Monthly Income Report | For the month ended %s, %s", month, year));
 		frame.setAlwaysOnTop(true);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
+
 		contentPane.setLayout(new BorderLayout(0, 0));
 
 		topBorder = new JPanel();
 		topBorder.setBackground(borderColor);
 		contentPane.add(topBorder, BorderLayout.NORTH);
 
-		JPanel leftBorder = new JPanel();
+		leftBorder = new JPanel();
 		leftBorder.setBackground(borderColor);
 		contentPane.add(leftBorder, BorderLayout.WEST);
 
-		JPanel bottomBorder = new JPanel();
+		bottomBorder = new JPanel();
 		bottomBorder.setBackground(borderColor);
 		contentPane.add(bottomBorder, BorderLayout.SOUTH);
 
-		JPanel rightBorder = new JPanel();
+		rightBorder = new JPanel();
 		rightBorder.setBackground(borderColor);
 		contentPane.add(rightBorder, BorderLayout.EAST);
 
-		JPanel incomeStatementPanel = new JPanel();
+		incomeStatementPanel = new JPanel();
 		incomeStatementPanel.setBackground(new Color(255, 255, 255));
 		contentPane.add(incomeStatementPanel, BorderLayout.CENTER);
-		incomeStatementPanel.setLayout(new MigLayout("", "[][][][][][][][][][][][][][][]", "[][][]"));
-		
-		Component verticalStrut = Box.createVerticalStrut(25);
-		incomeStatementPanel.add(verticalStrut, "cell 1 0");
-		
-		Component horizontalStrut = Box.createHorizontalStrut(25);
-		incomeStatementPanel.add(horizontalStrut, "cell 0 1");
-		
-		JLabel lblNewLabel = new JLabel("Income Statement");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
-		incomeStatementPanel.add(lblNewLabel, "cell 1 1");
-		
-		Component rigidArea = Box.createRigidArea(new Dimension(300, 40));
-		incomeStatementPanel.add(rigidArea, "cell 2 1 11 2");
-		
-		JLabel atcLogo = new JLabel("");
-		atcLogo.setIcon(new ImageIcon("C:\\Users\\User\\eclipse-workspace\\TuitionCenterManagementSystem\\src\\atcBannerWhite.png"));
-		incomeStatementPanel.add(atcLogo, "cell 13 1 1 2");
-		
-		JLabel lblNewLabel_1 = new JLabel(String.format("for the month ended %s, %s", month, year));
-		lblNewLabel_1.setFont(new Font("SansSerif", Font.BOLD, 12));
-		incomeStatementPanel.add(lblNewLabel_1, "cell 1 2");
+		incomeStatementPanel.setLayout(null);
+
+		incomeStatementLabel = new JLabel("Income Statement");
+		incomeStatementLabel.setBounds(45, 40, 302, 40);
+		incomeStatementLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
+		incomeStatementPanel.add(incomeStatementLabel);
+
+		atcLogo = new JLabel("");
+		atcLogo.setHorizontalAlignment(SwingConstants.RIGHT);
+		atcLogo.setBounds(610, 40, 200, 70);
+		atcLogo.setIcon(new ImageIcon("src//atcBannerWhite.png"));
+		incomeStatementPanel.add(atcLogo);
+
+		monthEndedlabel = new JLabel(String.format("for the month ended %s, %s", month, year));
+		monthEndedlabel.setBounds(45, 85, 334, 20);
+		monthEndedlabel.setFont(new Font("SansSerif", Font.BOLD, 16));
+		incomeStatementPanel.add(monthEndedlabel);
 
 		frame.setContentPane(contentPane);
 		frame.setVisible(true);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
