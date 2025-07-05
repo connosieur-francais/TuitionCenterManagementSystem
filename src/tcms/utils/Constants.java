0f4c72a59c8b46ef9634268565bad11b59ce7f65
ReasonalWork <reasonalworkplace@gmail.com>
2025-07-05 17:21:04 +0800

    edited constants

--- src/tcms/utils/Constants.java
+++ src/tcms/utils/Constants.java
@@ -3,7 +3,7 @@ package tcms.utils;
 import java.awt.Color;
 
 public class Constants {
-	// Color Scheme
+    // Color Scheme
     public static final Color CANVAS_COLOR = new Color(30, 33, 36);
     public static final Color BLURPLE = new Color(88, 101, 242);
     public static final Color BLURPLE_HOVER = new Color(79, 82, 196);
@@ -14,12 +14,12 @@ public class Constants {
     public static final Color TEXT_COLOR = Color.WHITE;
     public static final Color BORDER_COLOR = new Color(43, 45, 49);
     public static final Color DARK_GRAY = new Color(43, 45, 49);
-    
-	// FILE NAMES
-    public static final String USERS_CSV = "src/users.csv";
-    public static final String RECEPTIONISTS_CSV = "src/receptionists.csv";
-    public static final String TUTORS_CSV = "src/tutors.csv";
-    public static final String STUDENTS_CSV = "src/students.csv";
-    public static final String ADMINS_CSV = "src/admins.csv";
-    public static final String PAYMENTS_CSV = "src/payments.csv";
+
+    // File paths (relative to root folder or build path)
+    public static final String USERS_CSV = "tcms/data/users.csv";
+    public static final String RECEPTIONISTS_CSV = "tcms/data/receptionists.csv";
+    public static final String TUTORS_CSV = "tcms/data/tutors.csv";
+    public static final String STUDENTS_CSV = "tcms/data/students.csv";
+    public static final String ADMINS_CSV = "tcms/data/admins.csv";
+    public static final String PAYMENTS_CSV = "tcms/data/payments.csv";
 }
