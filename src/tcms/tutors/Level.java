4f9c7ff3a6a6ef5887799944a83a985d5d2a8640
ReasonalWork <reasonalworkplace@gmail.com>
2025-07-08 15:23:50 +0800

    Assign Subject for tutor COMPLETE

--- src/tcms/tutors/Level.java
+++ src/tcms/tutors/Level.java
@@ -1,27 +1,27 @@
-package tcms.tutors;
-
-public class Level {
-	private int levelID;
-	private String level;
+	package tcms.tutors;
 	
-	public Level(int levelID, String level) {
-		setLevelID(levelID);
-		setLevel(level);
-	}
+	public class Level {
+		private int levelID;
+		private String level;
+		
+		public Level(int levelID, String level) {
+			setLevelID(levelID);
+			setLevel(level);
+		}
+		
+		public int getLevelID() {
+			return levelID;
+		}
 	
-	public int getLevelID() {
-		return levelID;
+		public String getLevel() {
+			return level;
+		}
+	
+		public void setLevelID(int levelID) {
+			this.levelID = levelID;
+		}
+	
+		public void setLevel(String level) {
+			this.level = level;
+		}
 	}
-
-	public String getLevel() {
-		return level;
-	}
-
-	public void setLevelID(int levelID) {
-		this.levelID = levelID;
-	}
-
-	public void setLevel(String level) {
-		this.level = level;
-	}
-}
